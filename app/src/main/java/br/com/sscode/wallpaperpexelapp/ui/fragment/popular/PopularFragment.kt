c1c5ed11e6ac52f536e9ac1e471d821b94a567e1
rodrigossantos <rodrigossantos@brq.com>
2023-03-10 14:11:22 -0300

    [FEATURE] - Creating Popular Fragment Adapter, defining the item view of PhotoViewHolder

--- app/src/main/java/br/com/sscode/wallpaperpexelapp/ui/fragment/popular/PopularFragment.kt
+++ app/src/main/java/br/com/sscode/wallpaperpexelapp/ui/fragment/popular/PopularFragment.kt
@@ -5,13 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import br.com.sscode.core.model.PhotoDomain
 import br.com.sscode.wallpaperpexelapp.databinding.FragmentPopularBinding
+import br.com.sscode.wallpaperpexelapp.ui.fragment.adapter.photoadapter.PhotoAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PopularFragment : Fragment() {
 
     private lateinit var binding: FragmentPopularBinding
+    private lateinit var photoAdapter: PhotoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +27,21 @@ class PopularFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        photoAdapter = PhotoAdapter(::detail)
+        val gridLayoutManager = GridLayoutManager(requireContext(), 3)
+        with(binding.recyclerView) {
+            scrollToPosition(0)
+            layoutManager = gridLayoutManager
+            setHasFixedSize(true)
+            adapter = photoAdapter
+        }
+    }
+
+    private fun detail(photoDomain: PhotoDomain) {
+
     }
 }
