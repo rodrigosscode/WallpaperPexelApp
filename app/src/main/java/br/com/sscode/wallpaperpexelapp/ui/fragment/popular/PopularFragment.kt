225940eac2021c4d6830a1de50568057ef57cd2d
rodrigossantos <rodrigossantos@brq.com>
2023-03-13 14:28:13 -0300

    [FEATURE] - Creating ViewModel to Load Data from GetPopularUseCase, Creating Pulse Animation to Loading State

--- app/src/main/java/br/com/sscode/wallpaperpexelapp/ui/fragment/popular/PopularFragment.kt
+++ app/src/main/java/br/com/sscode/wallpaperpexelapp/ui/fragment/popular/PopularFragment.kt
@@ -4,18 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.GridLayoutManager
 import br.com.sscode.core.model.PhotoDomain
 import br.com.sscode.wallpaperpexelapp.databinding.FragmentPopularBinding
 import br.com.sscode.wallpaperpexelapp.ui.fragment.adapter.photoadapter.PhotoAdapter
+import br.com.sscode.wallpaperpexelapp.ui.fragment.popular.viewmodel.PopularViewModel
+import br.com.sscode.wallpaperpexelapp.util.animationCancel
+import br.com.sscode.wallpaperpexelapp.util.pulseAnimation
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class PopularFragment : Fragment() {
 
     private lateinit var binding: FragmentPopularBinding
     private lateinit var photoAdapter: PhotoAdapter
+    private val popularViewModel: PopularViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +40,8 @@ class PopularFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
+        observeLoadingState()
+        fetchWallpapers()
     }
 
     private fun initAdapter() {
@@ -38,6 +52,36 @@ class PopularFragment : Fragment() {
             layoutManager = gridLayoutManager
             setHasFixedSize(true)
             adapter = photoAdapter
+        }
+    }
+
+    private fun fetchWallpapers() = lifecycleScope.launch {
+        viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            popularViewModel.popularWallpapers().collect { pagingData ->
+                photoAdapter.submitData(pagingData)
+            }
+        }
+    }
+
+    private fun observeLoadingState() = lifecycleScope.launch {
+        photoAdapter.loadStateFlow.collectLatest { loadState ->
+            when (loadState.refresh) {
+                is LoadState.Loading -> {
+                    binding.imagePulseAnimation.visibility = View.VISIBLE
+                    binding.imagePulseAnimation.pulseAnimation()
+                }
+                is LoadState.NotLoading -> {
+                    binding.imagePulseAnimation.visibility = View.GONE
+                    binding.imagePulseAnimation.animationCancel()
+                }
+                is LoadState.Error -> {
+                    Toast.makeText(
+                        requireContext(),
+                        "Try again later",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
         }
     }
 
